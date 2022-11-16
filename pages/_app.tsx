5b8490bae72286a1594cf83279b585f5e79dc46b
tkyung05 <tkyung6761@gmail.com>
2022-11-16 12:29:39 +0900

    set: resetCSS + 라이브러리 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import type { AppProps } from "next/app";
+import React from "react";
+import { GlobalStyle } from "../styles/global-css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
