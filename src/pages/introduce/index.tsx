e817518cbd36d9fa7a67dcad3abc3499fdf98232
tkyung05 <tkyung6761@gmail.com>
2022-11-16 17:02:10 +0900

    chore: react-page-full 라이브러리 타입 설정

---
+++ src/pages/introduce/index.tsx
@@ -0,0 +1,11 @@
+import { FullPage, Slide } from "react-full-page";
+
+export function Introduce() {
+  return (
+    <FullPage>
+      <Slide>
+        <></>
+      </Slide>
+    </FullPage>
+  );
+}
